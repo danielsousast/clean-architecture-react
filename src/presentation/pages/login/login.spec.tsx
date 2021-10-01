f30a4d7f90b524102d9cfcfb19c2e5e69f13a2aa
Daniel Sousa <daielsousa@Daniels-iMac-Pro.local>
2021-10-01 10:33:17 -0300

    test: ensure start login with submit disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,11 +3,13 @@ import { render } from "@testing-library/react";
 import Login from "./login";
 
 describe("Login Component", () => {
-  test("Should not render form loading and error on start", () => {
+  test("Should start with initial state", () => {
     const { getByTestId } = render(<Login />);
 
     const errorWrap = getByTestId("error-wrap");
-
     expect(errorWrap.childElementCount).toBe(0);
+
+    const submit = getByTestId("submit") as HTMLButtonElement;
+    expect(submit.disabled).toBe(true);
   });
 });
